e20f65e6d0ce42ed2b5b32c5df9d8e0989e828c5
shoaibquraishi06 <dxshoaib51@gmail.com>
2025-12-07 20:50:19 +0530

    updaterui

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connectDB() {
 
     try {
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGODB_URI)
 
         console.log("Connected to MongoDB");
 
